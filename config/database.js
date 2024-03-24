26df23248a6c1a51d144be8b7669f3558e9255ab
SubhajitBar <55270770+SubhajitBar@users.noreply.github.com>
2024-03-24 17:46:41 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+// Database Connection
 export const connectDB = async () => {
     try {
         const { connection } = await mongoose.connect(process.env.MONGO_URI);
