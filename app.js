08964e87befdfdafb299c8e2a676414d86113b55
SubhajitBar <pvt.subha@gmail.com>
2024-03-24 23:59:06 +0530

    debugging

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use("/api", dataRoutes);
 
 export default app;
 
-app.get("/", (req, res) => res.send(
+app.get("/hint", (req, res) => res.send(
     `<h5>enter /api/data to get all data</h5>`
     // `<h5>enter /api/data/10 to get 10 data</h5>`
     )
